1965e8d0fc734d1d4ad0a716da8315978fea0157
zozer <18ml253@esisalama.org>
2023-06-02 10:49:11 +0200

    modified:   server/package.json 	modified:   server/server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const app = require('./app')
 
-const port = 3000
+const port = 5000
 
 app.listen(port, () => {
     console.log(`Le serveur est en cours d'exécution sur le port ${port}: \ncliquez ici pour ouvrir l'application http://localhost:3000`);
